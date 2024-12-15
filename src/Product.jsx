a1cdd9f54ae003a103ba12f0f991bb0b305ec973
shravani delta student <shravaniavula03@gmail.com>
2024-12-15 22:22:30 +0530

    add products styling

--- src/Product.jsx
+++ src/Product.jsx
@@ -1,10 +1,13 @@
-function Product(){
-return(
-    <div className="Product">
-        <h3>Product title</h3>
-        <h5>Product Description</h5>
-    </div>
-)
+import "./product.css";
+
+function Product({title,price}) {
+     // Ensure props.title is logged correctly
+    return (
+        <div className="Product">
+            <h3>{title}</h3>
+            <h5>Price : {price/2}</h5>
+        </div>
+    );
 }
 
 export default Product;
--- src/ProductTab.jsx
+++ src/ProductTab.jsx
@@ -1,16 +1,13 @@
 import Product from "./Product.jsx";
 
-
-function ProductTab(){
-
+function ProductTab() {
     return (
         <>
-        <Product/>
-        <Product/>
-        <Product/>
-    
+            <Product title="phone" price={30000} />
+            <Product title="laptop" price={40000} />
+            <Product title="pen" price={10}/>
         </>
     );
-    
 }
+
 export default ProductTab;
